515fe9fa9d737ee8e395c52070e0ae22ba0b5c48
McKenzie Fiege <mckenziefiege@McKenzies-MacBook-Pro.local>
2018-09-11 18:18:00 -0400

    updated methods in the user class

--- lib/models/list.rb
+++ lib/models/list.rb
@@ -1,18 +1,8 @@
-class ListItems < ActiveRecord::Base
+class ListItem < ActiveRecord::Base
   belongs_to :user
   belongs_to :movie
 
-  def show_a_users_lists
-    #will show the user all of their list/queues.
-  end
 
-  def show_movies_in_a_list
-    #will show all of the movies in a given list
-  end
-
-  def remove_movie
-    #removes a movie from their list/queue.
-  end
   #
   # def add_movie(user_id, movie_id)
   #   #adds a movie to their list/queue.
--- lib/models/user.rb
+++ lib/models/user.rb
@@ -2,17 +2,19 @@ class User < ActiveRecord::Base
   has_many :list_items
   has_many :movies, through: :list_items
 
-
   def show_movies_in_user_list
     #will show all movies in a user's list
-    ListItems.all.select do |list|
-      list.user_id == self
+    movies.map do |movie|
+      movie.title
     end
   end
 
-  def remove_movie_in_user_list
+  def remove_movie_in_user_list(movie)
     #removes a movie from their list/queue.
-
+    found_list_item = self.list_items.find do |list_item|
+      list_item.movie.title == movie
+    end
+    found_list_item.destroy
   end
 
   # def add_movie_to_database(movie_id)
