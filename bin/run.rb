92aa60709603bc8dd94209bee397cdbb62f85ca3
McKenzie Fiege <mckenziefiege@McKenzies-MacBook-Pro.local>
2018-09-12 11:40:16 -0400

    Added TTY prompt to run.rb

--- bin/run.rb
+++ bin/run.rb
@@ -1,20 +1,43 @@
 require_relative '../config/environment'
 require_relative '../snippets/cli_methods.rb'
 
-introduction
 
-user_input = gets.chomp
+puts "Hello, welcome to MovieQueueApp™."
+prompt = TTY::Prompt.new
+one_or_two = prompt.select("Choose '1' if you're an existing user or '2' to create a new account:", %w(1 2))
 
-case user_input
-when user_input == '1'
-    option_one
-  when user_input =='2'
-    option_two
-  when
-    puts 'Invalid input'
-  end
+if one_or_two == '1'
+   user_name = option_one
+ elsif one_or_two == '2'
+   user_name = option_two
 end
 
+options = {
+  add: "Choose 'add' to add movie to your queue.", remove: "Choose 'remove' to removie a movie from your queue.",
+  random: "Choose 'random' to select a random movie from your queue.", all: "Choose 'all' to see all movies in your queue.",
+  genre: "Choose 'genre', to show all movies in your queue by genre", movies: "Choose 'movies' to show all movies in database."
+}
+user_choice = prompt.select("Welcome #{user_name.name}. What would you like to do?", options.values)
+
+if user_choice = options.values[0]
+  title = prompt.ask('Enter a movie title:')
+  genre = prompt.select("Select a genre:", %w(Comedy Sci-Fi Horror Romance Action Thriller Drama Mystery Crime Animation Adventure Fantasy Comedy-Romance Action-Comedy SuperHero))
+  release_year = prompt.ask("Enter movie's release year:")
+  user_name.add_movie_to_database_and_queue(title, genre, release_year)
+end
+# introduction
+
+# user_input = gets.chomp
+
+# case user_input
+# when user_input == '1'
+#     option_one
+#   when user_input =='2'
+#     option_two
+#   when
+#     puts 'Invalid input'
+#   end
+# end
 
   # user_input = gets.chomp
   # if user_input == "1"
--- snippets/cli_methods.rb
+++ snippets/cli_methods.rb
@@ -5,22 +5,29 @@ def introduction
 end
 
 def option_one
-  puts "Please enter your username"
+  puts "Please enter your username:"
   name = gets.chomp
   User.all.find_by(name: name)
-  user_options(name)
 end
 
 def option_two
-  puts "Please enter a new username"
-  new_name = gets.chomp
-  User.create(name: new_name)
+  puts "Please enter a new username:"
+  name = gets.chomp
+  User.create(name: name)
 end
+
+options = {
+  add: "Choose 'add' to add movie to your queue.", remove: "Choose 'remove' to removie a movie from your queue.",
+  random: "Choose 'random' to select a random movie from your queue.", all: "Choose 'all' to see all movies in your queue.",
+  genre: "Choose 'genre', to show all movies in your queue by genre", movies: "Choose 'movies' to show all movies in database."
+}
 
 def user_options(name)
   puts "Welcome #{name}! what would you like to do?
   -Type 'add' to add movie to your queue.
   -Type 'remove' to remove a movie from your queue.
   -Type 'random' to choose a random movie from your queue.
-  -Type 'all' to see all movies in your queue."
+  -Type 'all' to see all movies in your queue.
+  -Type 'genre' to show movies in your queue by genre.
+  -Type 'movies' to show all movies in database." #think of another word besides database
 end
