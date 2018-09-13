6ee50478c361563e88098ee48d3bb318a85cafd4
McKenzie Fiege <mckenziefiege@McKenzies-MacBook-Pro.local>
2018-09-13 11:27:52 -0400

    Added colorize gem and added color to prompts

--- bin/run.rb
+++ bin/run.rb
@@ -1,42 +1,43 @@
 require_relative '../config/environment'
 require_relative '../snippets/cli_methods.rb'
 
+
 introduction
 sleep(1)
 prompt = TTY::Prompt.new
 
 user_name =  get_user_name
 
-puts  "Hey #{user_name.name}!"
+puts  "Hey #{user_name.name}!".blue.bold
 sleep(1)
 
 while true
   sleep(1)
-  user_choice = prompt.select("What would you like to do?", options.values)
+  user_choice = prompt.select("What would you like to do?".blue.bold, options.values)
   if user_choice == options.values[0]
-    title = prompt.ask('Enter a movie title:')
-    genre = prompt.select("Select a genre:", genre_array)
-    release_year = prompt.ask("Enter movie's release year:")
+    title = prompt.ask('Enter a movie title:'.blue)
+    genre = prompt.select("Select a genre:".blue, genre_array)
+    release_year = prompt.ask("Enter movie's release year:".blue)
     user_name.add_movie_to_database_and_queue(title, genre, release_year)
 
   elsif user_choice == options.values[1]
-     movie = prompt.ask('Enter a movie title to remove:')
+     movie = prompt.ask('Enter a movie title to remove:'.blue)
      if user_name.show_movies_in_user_list.empty?
-       puts "YOU DO NOT HAVE ANY MOVIES SAVED IN YOUR QUEUE"
+       puts "YOU DO NOT HAVE ANY MOVIES SAVED IN YOUR QUEUE".red.bold
      else
        user_name.remove_movie_in_user_list(movie)
      end
 
   elsif user_choice == options.values[2]
     if user_name.show_movies_in_user_list.empty?
-      puts "YOU DO NOT HAVE ANY MOVIES SAVED IN YOUR QUEUE"
+      puts "YOU DO NOT HAVE ANY MOVIES SAVED IN YOUR QUEUE".red.bold
     else
       puts user_name.pick_random_movie.title
     end
 
   elsif user_choice == options.values[3]
     if user_name.show_movies_in_user_list.empty?
-      puts "YOU DO NOT HAVE ANY MOVIES SAVED IN YOUR QUEUE"
+      puts "YOU DO NOT HAVE ANY MOVIES SAVED IN YOUR QUEUE".red.bold
     else
       puts user_name.show_movies_in_user_list
     end
@@ -45,7 +46,7 @@ while true
     genre = prompt.select("Select a genre:", genre_array)
     movies_with_genre = user_name.show_by_genre(genre)
     if movies_with_genre.empty?
-      puts "YOU DO NOT HAVE ANY MOVIES SAVED WITH THIS GENRE"
+      puts "YOU DO NOT HAVE ANY MOVIES SAVED WITH THIS GENRE".red.bold
     else
       puts movies_with_genre
     end
@@ -54,7 +55,7 @@ while true
     puts Movie.show_all_movies_in_database
 
   elsif user_choice == options.values[6]
-    puts "Thanks for using MovieQueueApp™! Goodbye!"
+    puts "Thanks for using MovieQueueApp™! Goodbye!".blue.bold
     break
   end
 end
--- snippets/cli_methods.rb
+++ snippets/cli_methods.rb
@@ -1,16 +1,16 @@
 def introduction
-  puts "Hello, welcome to MovieQueueApp™."
+  puts "Hello, welcome to MovieQueueApp™.".blue.bold
 end
 
 def get_user_name
-puts "Please enter your username:"
+puts "Please enter your username:".blue.bold
   while user_input = gets.chomp.downcase
     if user_input.length >= 4
       name = user_input
       return User.all.find_or_create_by(name: name)
       break
     else
-      puts "INVALID INPUT: USERNAME MUST BE GREATER THAN 4 CHARACTERS. PLEASE TRY AGAIN"
+      puts "INVALID INPUT: USERNAME MUST BE GREATER THAN 4 CHARACTERS. PLEASE TRY AGAIN".red
     end
   end
 end
@@ -25,5 +25,5 @@ def options
 end
 
 def genre_array
-  array = ["Comedy", "Sci-Fi", "Horror", "Romance", "Action", "Thriller", "Drama", "Mystery", "Crime", "Animation", "Adventure", "Fantasy", "Comedy-Romance", "Action-Comedy", "SuperHero"]
+  array = ["Comedy", "Science Fiction", "Horror", "Romance", "Action", "Thriller", "Drama", "Mystery", "Crime", "Animation", "Adventure", "Fantasy", "Comedy-Romance", "Action-Comedy", "SuperHero"]
 end
