b0b0041696c9dbc8407329aca86f7e3dac749f91
Kat Michaela <k@gothfem.me>
2018-09-13 18:06:29 -0400

    added comments

--- bin/run.rb
+++ bin/run.rb
@@ -1,7 +1,7 @@
 require_relative '../config/environment'
 
-Program_user = initialize_user
+Program_user = initialize_user # sets constant to return value from this method, which will be an instance of the User class if login/create account methods are valid.
 
-loop do
+loop do # main program loop
   program_loop
 end
